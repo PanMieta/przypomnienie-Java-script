958e55bd626f51ee4ee264b49e227541ef5fe128
PanMieta <133598451+PanMieta@users.noreply.github.com>
2023-09-28 13:27:53 +0200

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,61 @@
+//Zad1
+var a = 5;
+
+var b = 10;
+
+var suma = a+b;
+
+var roznica = a-b;
+
+var iloczyn = a*b;
+
+var iloraz = a/b;
+
+	document.write(suma,"- Suma<br>");
+	document.write(roznica,"- Różnica<br>");
+	document.write(iloczyn,"- Iloczyn<br>");
+	document.write(iloraz,"- Iloraz<br>");
+	
+//Zad2	
+var liczba = 5; 
+
+var tekst = "test"; 
+
+var ulamek = 0.7;
+
+var logiczna = true;
+
+	document.write("<br>",logiczna, "-Zmienna logiczna<br>");
+
+	document.write (ulamek,"-Zmienna ułamkowa<br>"); 
+
+	document.write (liczba,"-Zmienna liczbowa całkowita<br>",tekst,"-Zmienna tekstowa<br>");
+	
+//Zad3
+	
+	document.write("<ul>","<li>",suma,"</li>","<li>",iloraz,"</li>","<li>",iloczyn,"</li>","<li>",roznica,"</li>","</ul><br>");
+	
+//Zad4
+
+let x = 6;
+let z = Math.pow(x,2);
+document.write("Potega: ", z,"<br>");
+
+//Zad5
+
+var dziesietny = 12;
+var osemkowy =037;
+var szesnastkowy =0xACB;
+var notacjawykladnicza = 0.12e-2;
+
+//Zad6
+
+var li1 = 4;
+var li2 = 5;
+var wynikmn = li1*li2;
+var wynikdz = wynikmn/li2;
+
+	document.write("<h1>Mnożenie: </h1>",li1," * ",li2," = ",wynikmn,"<br><h1>Dzielenie: </h1>",wynikmn," : ",li2," = ",wynikdz);
+
+
+	
